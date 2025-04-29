59b5b9a6e05e9857819394f661400c7e935af2f0
rayssacruz <rayssa0433@gmail.com>
2025-04-29 13:16:16 +0000

    exemplo tres

--- main.js
+++ main.js
@@ -24,3 +24,28 @@
   somar(30,12)
   somar(19,289)
   */
+ 
+ 
+  
+  
+  // exemplo 3
+  /*
+  function raizQuadrada(numero) {
+      console.log(Math.sqrt(numero))
+  }
+    
+    function potenciacao(valor1, valor2) {
+      console.log(Math.pow(valor1, valor2))
+  }
+    
+    function multiplicacao(valor1, valor2) {
+      console.log(valor1 * valor2)
+  }
+    
+  let numero1 = Number(prompt("Digite o número 1: "))
+  let numero2 = Number(prompt("Digite o número 2: "))
+    
+  raizQuadrada(numero1)
+  potenciacao(numero1, numero2)
+  multiplicacao(numero2, numero1)
+  */
