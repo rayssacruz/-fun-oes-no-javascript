4afbc338418230b2d9068d83a2fbb21919ed0377
rayssacruz <rayssa0433@gmail.com>
2025-04-29 13:19:29 +0000

    quarto coiso

--- main.js
+++ main.js
@@ -45,7 +45,32 @@
   let numero1 = Number(prompt("Digite o número 1: "))
   let numero2 = Number(prompt("Digite o número 2: "))
     
-  raizQuadrada(numero1)
+   raizQuadrada(numero1)
   potenciacao(numero1, numero2)
   multiplicacao(numero2, numero1)
   */
+ 
+ 
+ // exemplo 4
+  /*
+  function raizQuadrada(numero) {
+      return Math.sqrt(numero)
+  }
+    
+    function potenciacao(valor1, valor2) {
+      return Math.pow(valor1, valor2)
+  }
+    
+    function multiplicacao(valor1, valor2) {
+      return valor1 * valor2
+  }
+    
+  let numero1 = Number(prompt("Digite o número 1: "))
+  let numero2 = Number(prompt("Digite o número 2: "))
+  
+  let raiz1 = raizQuadrada(numero1)
+  let raiz2 = raizQuadrada(numero2)
+  console.log(raiz1 , raiz2)
+  */
+
+  
