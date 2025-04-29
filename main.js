daf94eeddb6e498495563e5bbb4d57d5483895fe
rayssacruz <rayssa0433@gmail.com>
2025-04-29 13:14:48 +0000

    segundo exemplo

--- main.js
+++ main.js
@@ -4,7 +4,23 @@
       alert("Seja bem-vindo(a)!")
       console.log("Seja bem-vindo(a)!")
       console.log("Você é muito legal!")
-    }
+     }
     
     mensagem()
-  */s
+  */
+  
+  
+  
+  // exemplo 2
+  
+  /*
+  function somar(numero1, numero2) {
+      console.log(numero1 + numero2)
+  }
+    
+  let numero1 = 20;
+  let numero2 = 30;
+  somar(numero1, numero2)
+  somar(30,12)
+  somar(19,289)
+  */
