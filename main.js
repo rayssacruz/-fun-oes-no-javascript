c8262ced9758ccadfff151a18917a7ed36293aa2
rayssacruz <rayssa0433@gmail.com>
2025-04-29 13:04:32 +0000

    Primeiro coiso

---
+++ main.js
@@ -0,0 +1,10 @@
+// exemplo 1
+  /*
+  function mensagem() {
+      alert("Seja bem-vindo(a)!")
+      console.log("Seja bem-vindo(a)!")
+      console.log("Você é muito legal!")
+    }
+    
+    mensagem()
+  */s
